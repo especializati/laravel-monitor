3e1eb1c45752327635b0b4e65eaa9fd9a3fff245
Carlos Ferreira <carlos@especializati.com.br>
2023-04-25 15:12:59 -0300

    feat: define relationships

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Check extends Model
 {
     use HasFactory;
 
     protected $fillable = ['status_code', 'response_body'];
+
+    public function endpoint(): BelongsTo
+    {
+        return $this->belongsTo(Endpoint::class);
+    }
 }
--- app/Models/Endpoint.php
+++ app/Models/Endpoint.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Endpoint extends Model
 {
     use HasFactory;
 
     protected $fillable = ['endpoint', 'frequency', 'next_check'];
+
+    public function site(): BelongsTo
+    {
+        return $this->belongsTo(Site::class);
+    }
+
+    public function checks(): HasMany
+    {
+        return $this->hasMany(Check::class);
+    }
 }
