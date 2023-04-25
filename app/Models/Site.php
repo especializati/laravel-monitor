5bf69d0757ab4cd5e98b4fcd9fd066288a5c1a30
Carlos Ferreira <carlos@especializati.com.br>
2023-04-25 16:42:23 -0300

    feat: filter sites by global scope

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -2,17 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Site extends Model
 {
     use HasFactory, HasUuids;
 
     protected $fillable = ['url', 'user_id'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', Auth::user()->id);
+        });
+    }
 
     public function user(): BelongsTo
     {
