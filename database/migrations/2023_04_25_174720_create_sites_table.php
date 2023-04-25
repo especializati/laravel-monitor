d0c455b27d48d76cb5a3863fb6d5ad3594ba0fbc
Carlos Ferreira <carlos@especializati.com.br>
2023-04-25 14:53:33 -0300

    feat: create model and table sites

--- database/migrations/2023_04_25_174720_create_sites_table.php
+++ database/migrations/2023_04_25_174720_create_sites_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sites', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('url');
+            $table->string('user_id')->index();
             $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users');
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sites');
     }
 };
