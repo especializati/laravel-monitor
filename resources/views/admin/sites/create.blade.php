6e4d86c8a010010d45d9a2659781a4882d3bef7c
Carlos Ferreira <carlos@especializati.com.br>
2023-04-25 16:26:08 -0300

    refactor: reuse resources

--- resources/views/admin/sites/create.blade.php
+++ resources/views/admin/sites/create.blade.php
@@ -1,15 +1,7 @@
 <h1>Cadastrar Novo Site</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="alert-error">
-            <p>{{ $error }}</p>
-        </div>
-    @endforeach
-@endif
+<x-alerts/>
 
 <form action="{{ route('sites.store') }}" method="post">
-    @csrf()
-    <input type="text" name="url" placeholder="URL">
-    <button type="submit">Enviar</button>
+    @include('admin/sites/partials/form')
 </form>
