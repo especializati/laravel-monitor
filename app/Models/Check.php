9111ac8c56dba646c50bf793b375a6abe2eb9206
Carlos Ferreira <carlos@especializati.com.br>
2023-04-25 15:14:55 -0300

    refactor: use trait uuid

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Check extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = ['status_code', 'response_body'];
 
