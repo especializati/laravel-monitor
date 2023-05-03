d2a45505f30dda188c4b0c51dff2e4e96b19ab3d
Carlos Ferreira <carlos@especializati.com.br>
2023-05-03 16:39:50 -0300

    refactor: paginate results

--- app/Http/Controllers/Admin/SiteController.php
+++ app/Http/Controllers/Admin/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends Controller
 {
     public function index()
     {
-        $sites = Site::get();
+        $sites = Site::paginate();
 
         return view('admin/sites/index', compact('sites'));
     }
