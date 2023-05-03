a113bfd85a076dbee13d877cfef9b6555f9ad4f3
Carlos Ferreira <carlos@especializati.com.br>
2023-05-03 16:13:46 -0300

    fix: get old url

--- resources/views/admin/sites/partials/form.blade.php
+++ resources/views/admin/sites/partials/form.blade.php
@@ -1,3 +1,3 @@
 @csrf()
-<input type="text" name="url" placeholder="URL" value="{{ $site->url ?? old('site') }}">
+<input type="text" name="url" placeholder="URL" value="{{ $site->url ?? old('url') }}">
 <button type="submit">Enviar</button>
