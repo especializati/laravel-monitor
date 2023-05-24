6dce5710722cd353053c0be87f33391b90155c33
Carlos Ferreira <carlos@especializati.com.br>
2023-05-24 15:45:14 -0300

    refactor: apply SOLID

--- app/Jobs/EndpointCheckJob.php
+++ app/Jobs/EndpointCheckJob.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Client\Response;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
@@ -32,11 +33,25 @@ class EndpointCheckJob implements ShouldQueue
 
         $this->endpoint->checks()->create([
             'status_code' => $response->status(),
-            'response_body' => $response->successful() ? null : $response->body(),
+            'response_body' => $this->responseBody($response),
         ]);
 
         $this->endpoint->update([
-            'next_check' => now()->addMinutes($this->endpoint->frequency),
+            'next_check' => $this->nextCheck(),
         ]);
+    }
+
+    private function nextCheck()
+    {
+        return now()->addMinutes($this->endpoint->frequency);
+    }
+
+    private function responseBody(Response $response): string|null
+    {
+        if ($response->successful()) {
+            return null;
+        }
+
+        return (string) $response->body();
     }
 }
