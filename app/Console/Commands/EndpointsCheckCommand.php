ddb7be2b93f2754424a6b1d5e2c7cfa51fb0e9c6
Carlos Ferreira <carlos@especializati.com.br>
2023-05-24 15:56:44 -0300

    feat: create command run job check endpoints

---
+++ app/Console/Commands/EndpointsCheckCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\EndpointCheckJob;
+use App\Models\Endpoint;
+use Illuminate\Console\Command;
+
+class EndpointsCheckCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:endpoints-check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check all endpoints';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Endpoint::where('next_check', '<=', now())->each(function ($endpoint) {
+            EndpointCheckJob::dispatchSync($endpoint);
+        });
+    }
+}
