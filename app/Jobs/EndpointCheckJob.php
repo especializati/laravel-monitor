5c1d2c428285fc3b125a8677aa3dc05c5d1f15d2
Carlos Ferreira <carlos@especializati.com.br>
2023-05-24 15:22:41 -0300

    feat: create job for check endpoint

---
+++ app/Jobs/EndpointCheckJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Endpoint;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class EndpointCheckJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Endpoint $endpoint,
+    ) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $url = $this->endpoint->url();
+        $response = Http::get($url);
+
+        $this->endpoint->checks()->create([
+            'status_code' => $response->status(),
+            'response_body' => $response->body(),
+        ]);
+
+        $this->endpoint->update([
+            'next_check' => now()->addMinutes($this->endpoint->frequency),
+        ]);
+    }
+}
