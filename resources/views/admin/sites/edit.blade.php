77d5faddfb4faf4f8a9cf0021d2cf3ad3ef6aa4c
Carlos Ferreira <carlos@especializati.com.br>
2023-06-13 16:57:23 -0300

    fix: change type for submit

--- resources/views/admin/sites/edit.blade.php
+++ resources/views/admin/sites/edit.blade.php
@@ -17,7 +17,7 @@
                     <form action="{{ route('sites.destroy', $site->id) }}" method="post">
                         @csrf()
                         @method('DELETE')
-                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-600 hover:bg-red-700 focus:ring-red-900 p-4">Deletar</button>
+                        <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-600 hover:bg-red-700 focus:ring-red-900 p-4">Deletar</button>
                     </form>
                 </div>
             </div>
