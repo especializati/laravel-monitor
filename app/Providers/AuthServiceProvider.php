8cfb4cd31d4679e5335fd46a57dcdc6ce55add90
Carlos Ferreira <carlos@especializati.com.br>
2023-06-13 16:44:27 -0300

    feat: create gates owner

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Endpoint;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('owner', function (User $user, Model $model) {
+            return $user->id === $model->user_id;
+        });
+
+        Gate::define('ownerChecks', function (User $user, Endpoint $endpoint) {
+            return $user->id === $endpoint->site->user_id;
+        });
     }
 }
